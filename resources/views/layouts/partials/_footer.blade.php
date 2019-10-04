079983ec261682724d8c9a8394aa89c0d702a9c6
jisDev21 <jis@jisdev.com>
2019-10-05 00:37:00 +0200

    Configure app name

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="text-center">
 	<p>&copy; {{ date('Y') }} &middot; Laracarte by <a href="https://twitter.com/etsmo">@etsmo</a> - A Laramap clone app.</p>
 
-	<p><b>This app has benn built for learning purposes.</b></p>
+	<p><b>This app has been built for learning purposes.</b></p>
 
 
 </footer>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,3 +1,15 @@
+{{-- notre vue hérite de la vue \layouts\default.blade.php
+
+		@extends('layouts.default')
+
+	si on voulait que le titre de la fenetre web soit dynamique, on pourrait passer
+	une variable "title" pour chacune des vues et récupérer cette variable dans la vue
+	générale \layouts\default.blade.php
+
+		@extends('layouts.default', ['title' => 'Home']) 
+
+--}}
+
 @extends('layouts.default', ['title' => 'Home'])
 
 
